50a831a28b59040234ef27130a9953cbfa1a6a91
LeFab70 <kouonang2002@gmail.com>
2026-05-06 20:17:34 -0300

    Planificateur de repas refactoring

--- obj/Debug/net10.0-android/RecetteApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/RecetteApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecetteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5e9db5a5d3f5ec2d65ce32b66bdf6417b48caee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+96b8d041ec5cd53c5b29425d3e1bf4a0e88afa25")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecetteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecetteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
